832fac514e222deea82dcd0b77f18ad6a6c96205
Alice Jacka <jacka.alice@gmail.com>
2023-08-11 11:05:01 +1000

    Pause batch script if server exits non 0

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 node -v > NUL || echo [91mNode.js is not installed. Please install Node.js from https://nodejs.org/en/download/[0m && pause && exit
-node src\server.js
+node src\server.js || pause
