e662842d971d84b0869ff956c0d7da9e928043ce
Alice Jacka <jacka.alice@gmail.com>
2023-09-13 10:38:56 +1000

    Give the cmd window a title

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 @echo off
+title Golabel Automator
 node -v > NUL || echo [91mERROR: Node.js is not installed. Please install Node.js from https://nodejs.org/en/download/[0m && pause && exit
 node src\server.js %1 || pause
